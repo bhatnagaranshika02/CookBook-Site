6dec9604b289d25a71e1222859e5b44077c31211
Anshika Bhatnagar <bhatnagaranshika02@gmail.com>
2022-07-07 13:42:34 +0000

    Created two models in models.py:   1. Ingredients   2. Category Linked Category and Ingredients tables via foreign key concept.

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__ (self):
+        return self.name
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=255)
+    notes = TextField(max_length=255)
+
+    category = models.ForeignKey(Category, related_names="ingredients", on_delete=models.CASCADE)
+
+    def __str__ (self):
+        return self.name
+
+
